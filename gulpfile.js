e6c6720e7575835f3e788a2b2c55735a0907f93a
patrio <97680337+timothyme@users.noreply.github.com>
2022-02-18 17:08:41 +0100

    Gulp teamplay task

--- gulpfile.js
+++ gulpfile.js
@@ -7,15 +7,27 @@ function clean(cb) {
     // body omitted
     cb();
 }
-  
-// The `build` function is exported so it is public and can be run with the `gulp` command.
-// It can also be used within the `series()` composition.
-function build(cb) {
+
+function teamplaypk3(cb) {
+    gulp.src(['qw/__s_timer.cfg', 'qw/__s_teamplay.cfg'])
+		.pipe(zip('suddenteamplay.pk3'))
+		.pipe(gulp.dest('qw'))
+        .on('end', () => cb());
+}
+function teamplayzip(cb) {
     gulp.src(['qw/suddenteamplay.pk3', 'qw/suddenteamplay.cfg'])
-		.pipe(zip('suddenteamplay-hej.zip'))
+		.pipe(zip('suddenteamplay-latest.zip'))
 		.pipe(gulp.dest('dist'))
     cb();
 }
   
-  exports.build = build;
-  exports.default = gulp.series(clean, build);
+// The `build` function is exported so it is public and can be run with the `gulp` command.
+// It can also be used within the `series()` composition.
+function build(cb) {
+    // body omitted
+    cb();
+}
+
+exports.build = build;
+exports.teamplay = gulp.series(teamplaypk3, teamplayzip);
+exports.default = gulp.series(clean, build);
