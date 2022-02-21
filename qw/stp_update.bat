2b54a9db7b6bf1984a899f50a724b3b5b9c58ea6
patrio <97680337+timothyme@users.noreply.github.com>
2022-02-21 19:48:16 +0100

    include bat file if scared of exe

---
+++ qw/stp_update.bat
@@ -0,0 +1,6 @@
+@echo off
+echo "Downloading latest version of suddenteamplay.pk3..."
+curl https://github.com/Suddendeath-qw/suddenQuake/releases/download/latest/suddenteamplay.pk3 -L -o suddenteamplay.pk3 
+
+
+@pause
