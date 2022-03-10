9e3f106ab6dd127738b2af2d2ccad3e5d3210d74
Patric Nordmark <patric.nordmark@gmail.com>
2022-03-10 12:33:49 +0100

    typescript wip

--- logs/stats.ts
+++ logs/stats.ts
@@ -12,6 +12,68 @@ let players = [
     "mOlle"
 ]
 
+interface PlayerStats {
+    team: string,
+    name: string,
+    frags: number
+    net: number,
+    tk: number,
+    eff: number,
+    wp: {
+        axe?: number,
+        sg?: number,
+        ssg?: number,
+        ng?: number,
+        sng?: number
+        gl?: number,
+        rl?: number,
+        lg?: number
+    },
+    rl_skill: {
+        ad?: number,
+        dh?: number
+    },
+    armr_mhs: {
+        ga?: number,
+        ya?: number,
+        ra?: number,
+        mh?: number
+    },
+    powerups: {
+        q?: number,
+        p?: number,
+        r?: number
+    },
+    rl?: {
+        took: number,
+        killed: number,
+        dropped: number,
+        xfer?: number
+    },
+    lg?: {
+        took: number,
+        killed: number,
+        dropped: number,
+        xfer?: number
+    },
+    damage: {
+        tkn: number,
+        gvn: number,
+        ewep?: number,
+        tm?: number,
+        self?: number,
+        todie?: number
+    },
+    time: {
+        quad: number
+    },
+    streaks: {
+        frags: number,
+        quadrun: number
+    },
+    spawnfrag: number
+}
+
 class Player {
     name: string
     matches: number
@@ -20,7 +82,7 @@ class Player {
         this.matches = 0
     }
 
-    addMatchStats (stats) {
+    addPlayerStats (stats) {
         //
     }
 }
