e06b48edd673478f8c78633a9503588fa91319c5
Patric Nordmark <patric.nordmark@gmail.com>
2022-04-03 14:15:05 +0200

    mvd-to-json wip

--- logs/mvd-to-json.js
+++ logs/mvd-to-json.js
@@ -4,7 +4,7 @@ exports.MatchData = void 0;
 var fs = require("fs");
 var path = require("path");
 var crypto = require("crypto");
-var Mvd_Parser_1 = require("./Mvd_Parser");
+var clr = require("ansi-colors");
 /*
  * ====================================================================
  *
@@ -37,6 +37,39 @@ var emptyMatchDataObj = function () { return ({
     map: "",
     teams: {}
 }); };
+var CHARSET = {
+    10: 10,
+    16: 91,
+    17: 93,
+    18: 48,
+    19: 49,
+    20: 50,
+    21: 51,
+    22: 52,
+    23: 53,
+    24: 54,
+    28: 46,
+    135: 35,
+    // -128 applied here
+    143: 46,
+    144: 91,
+    145: 93,
+    156: 46,
+    // --------------------
+    157: 60,
+    158: 61,
+    159: 62
+};
+var STRIP_CHARS = [
+    0,
+    1,
+    2,
+    3,
+    4,
+    8,
+    9,
+    31, // US - Unit separator
+];
 /*
  * ====================================================================
  *
@@ -44,6 +77,96 @@ var emptyMatchDataObj = function () { return ({
  *
  * ====================================================================
  */
+var Mvd_Parser = /** @class */ (function () {
+    function Mvd_Parser(buf) {
+        this.buf = this.stripControlChars(buf);
+        // Get metadata
+        this.metadata = this.getMetadata(this.buf);
+        // Slice buffer (only need the stats in memory)
+        this.buf = this.getEndGameStats(this.buf);
+        this.parseMvd();
+    }
+    Mvd_Parser.prototype.getStats = function () {
+        // Create a string
+        var stats = this.metadata.join('\n');
+        stats = stats + '\n' + this.buf.toString();
+        //console.log(stats);
+        return stats;
+    };
+    Mvd_Parser.prototype.toString = function () {
+        return this.buf.toString();
+    };
+    Mvd_Parser.prototype.getMetadata = function (buf) {
+        var metadata;
+        var match;
+        var idxStart, idxEnd;
+        var str;
+        metadata = [];
+        // Mvd
+        idxStart = this.buf.indexOf("Server starts recording");
+        idxEnd = this.buf.indexOf(".mvd", idxStart) + 4;
+        str = this.buf.subarray(idxStart, idxEnd).toString();
+        if (match = str.match(/Server starts recording \([a-z]+\)\:\s(.+\.mvd)/mi)) {
+            metadata.push(match[0].replace(/[\r\n]+/g, ' ').trim());
+        }
+        // Matchdate
+        idxStart = this.buf.indexOf("matchdate:");
+        idxEnd = this.buf.indexOf("\n", idxStart);
+        str = this.buf.subarray(idxStart, idxEnd).toString();
+        if (match = str.match(/matchdate\: ([0-9]{4}\-[0-9]{2}\-[0-9]{2}\s[0-9]{2}\:[0-9]{2}\:[0-9]{2}\s[a-z]{0,5})/mi)) {
+            metadata.push(match[0]);
+        }
+        return metadata;
+    };
+    Mvd_Parser.prototype.getEndGameStats = function (buf) {
+        var idxMatchOver = buf.indexOf("The match is over");
+        var idxFinalScores = buf.indexOf("//finalscores");
+        var idxJsonStart = buf.indexOf('{"version":');
+        var idxEndStats = idxJsonStart > 0 ? idxJsonStart : idxFinalScores;
+        // Slice out end game stats to subarray
+        return buf.subarray(idxMatchOver, idxEndStats);
+    };
+    Mvd_Parser.prototype.stripControlChars = function (buf) {
+        var newBufferChars = [];
+        var i;
+        for (i = 0; i < buf.length; i++) {
+            if (STRIP_CHARS.includes(buf[i]))
+                continue;
+            newBufferChars.push(buf[i]);
+        }
+        return Buffer.from(newBufferChars);
+    };
+    Mvd_Parser.prototype.parseMvd = function () {
+        var buf = this.buf;
+        // Parse the stats quake chars
+        var i = 0;
+        for (i = 0; i < buf.length; i++) {
+            buf[i] = this.parseQuakeChar(buf[i], i, buf);
+        }
+    };
+    Mvd_Parser.prototype.parseQuakeChar = function (charcode, i, buf) {
+        // Normalize "red chars", secondary color in charset
+        // Probably the entire range 143-160 is -128, but not confirmed
+        if (charcode > 143 && charcode < 157)
+            charcode -= 128;
+        else if (charcode > 160 && charcode < 255)
+            charcode -= 128;
+        // Nothing wrong with normal ASCII-chars
+        if (charcode > 31 && charcode < 127)
+            return charcode;
+        else if (CHARSET.hasOwnProperty(charcode))
+            return CHARSET[charcode];
+        else {
+            var pre = buf.subarray(i - 5, i).toString();
+            var is = buf.subarray(i, i + 1).toString();
+            var suf = buf.subarray(i + 1, i + 5).toString();
+            console.log(i + ":" + charcode, pre, clr.bgRed(is), suf);
+            //console.log("%d: char (%d) not found, %s", i, charcode, helpstring)
+            return charcode;
+        }
+    };
+    return Mvd_Parser;
+}());
 var MatchData = /** @class */ (function () {
     function MatchData(m) {
         this.id = "";
@@ -71,7 +194,7 @@ var LineCursor = /** @class */ (function () {
         // .replace(/[\r\n]+/g, '')
         this.len = this.lines.length;
         this.n = 0;
-        console.log("lines", this.len);
+        //console.log("lines", this.len)
     }
     LineCursor.prototype.line = function () { return this.lines[this.n]; };
     LineCursor.prototype.next = function () { return this.n < this.len ? this.lines[++this.n] : null; };
@@ -84,7 +207,7 @@ var LineParser = /** @class */ (function () {
         this.re_MatchMvd = /^Server\sstarts\srecording\s\((?:memory|disk)\)\:\s(.+)$/i;
         this.re_MatchDate = /^matchdate\:\s([0-9]{4}\-[0-9]{2}\-[0-9]{2}\s[0-9]{2}\:[0-9]{2}\:[0-9]{2}\s[a-z]+)$/i;
         this.re_PlayerTeam = /^Team\s\[(.+)\]\:$/i;
-        this.re_Player = /^\_\s(.+)\:$/i;
+        this.re_Player = /^\#\s(.+)\:$/i;
         this.re_Player_1 = /^([0-9\-]+)\s\(([0-9\-]+)\)\s([0-9]+)\s([0-9.%]+)$/;
         this.re_Player_2 = /^Wp\:\s([a-z0-9.%]+)?(?:\s?\([0-9]+\/[0-9]+\))?\s?([a-z0-9.%]+)?\s?([a-z0-9.%]+)?\s?([a-z0-9.%]+)?\s?([a-z0-9.%]+)?$/i;
         this.re_Player_3 = /^([a-z\s\&]+)\:\s([a-z]+\:[0-9.]+)\s?([a-z]+\:[0-9.]+)?\s?([a-z]+\:[0-9.]+)?\s?([a-z]+\:[0-9.]+)?\s?([a-z]+\:[0-9.]+)?\s?([a-z]+\:[0-9.]+)?/i;
@@ -232,19 +355,21 @@ var LineParser = /** @class */ (function () {
  */
 function Log_ParseFile(fpath) {
     var fileInfo = path.parse(fpath);
-    //console.log(fileInfo)
+    console.log("  →", clr.white("parsing ".concat(fileInfo.name, ".mvd")));
     var buf = fs.readFileSync(fpath);
-    var mvd = new Mvd_Parser_1.Mvd_Parser(buf);
+    var mvd = new Mvd_Parser(buf);
     var endStats = mvd.getStats();
     //console.log(endStats);
     var lc = new LineCursor(Buffer.from(endStats));
     var md = new MatchData(emptyMatchDataObj());
     var lineParser = new LineParser(lc, md);
     var data = lineParser.parse();
-    console.log(data);
+    //console.log(data)
     // WriteJSON
     var outputPath = path.join(process.cwd(), fileInfo.name + ".json");
     fs.writeFileSync(outputPath, JSON.stringify(data, null, 2));
+    console.log("  →", clr.green("writing ".concat(fileInfo.name, ".json")));
+    console.log();
     //console.log(data)
 }
 function Log_ParseFiles(fpath) {
@@ -256,10 +381,10 @@ function Log_ParseFiles(fpath) {
 function main() {
     try {
         if (!process.argv[2])
-            throw new Error("usage: node logparser.mjs <logfile|logdir>");
+            throw new Error("usage: node mvd-to-json.js <demofile.mvd|demodir>");
         var fpath = path.join(process.cwd(), process.argv[2]);
         if (!fs.existsSync(fpath))
-            throw new Error("logfile or directory not found");
+            throw new Error("demofile or directory not found");
         var fstat = fs.lstatSync(fpath);
         if (fstat.isDirectory())
             Log_ParseFiles(fpath);
--- logs/mvd-to-json.ts
+++ logs/mvd-to-json.ts
@@ -1,7 +1,7 @@
 import * as fs from "fs"
 import * as path from "path"
 import * as crypto from "crypto"
-import {Mvd_Parser} from "./Mvd_Parser"
+import * as clr from "ansi-colors"
 import {PlayerData, MatchDataObj} from "./logtypes"
 /*
  * ====================================================================
@@ -38,6 +38,50 @@ const emptyMatchDataObj = ():MatchDataObj => ({
     teams: {}
 });
 
+const CHARSET = {
+    10: 10, // LF (LineFeed)
+
+    16: 91, // [ (gold [) 144-128 actually works here
+    17: 93, // [ (gold [) 145-128 actually works here
+    18: 48, // 0 gold??? NOT CONFIRMED
+    19: 49, // 1 gold??? NOT CONFIRMED
+    20: 50, // 2 gold??? NOT CONFIRMED
+    21: 51, // 3 gold??? NOT CONFIRMED
+    22: 52, // 4 gold??? NOT CONFIRMED
+    23: 53, // 5 gold??? NOT CONFIRMED
+    24: 54, // CAN becomes gold 6? 152-128???
+    28: 46, // . (. mid white slackers) 156-128 works here
+
+    135: 35, // # (square red?)
+
+    // -128 applied here
+    143: 46, // . (. mid gold?) 
+    144: 91, // [ (gold [)
+    145: 93, // ] (gold ])
+    156: 46, // . (. mid white?)
+    // --------------------
+
+    157: 60, // < (left <=)
+    158: 61, // = (middle in <===>)
+    159: 62, // > (right =>)
+
+    // 160 - 254
+    // Red characters
+    // space ! " #
+    // a-zA-Z
+    // 0-9 etc...
+};
+
+const STRIP_CHARS = [
+    0, // null
+    1, // SOH - Start of Header
+    2, // STX - Start of Text
+    3, // ETX - End of Text
+    4, // EOT - End of Transmission
+    8, // BS - Backspace
+    9, // HT - Horizontal Tab
+    31, // US - Unit separator
+];
 
 /*
  * ====================================================================
@@ -46,6 +90,116 @@ const emptyMatchDataObj = ():MatchDataObj => ({
  *
  * ====================================================================
  */
+
+class Mvd_Parser {
+    buf:Buffer
+
+    metadata: Array<string>
+
+    constructor (buf:Buffer) {
+        this.buf = this.stripControlChars(buf);
+
+        // Get metadata
+        this.metadata = this.getMetadata(this.buf);
+
+        // Slice buffer (only need the stats in memory)
+        this.buf = this.getEndGameStats(this.buf);
+        
+        this.parseMvd();
+    }
+
+    getStats() {
+        // Create a string
+        let stats = this.metadata.join('\n');
+        stats = stats + '\n' + this.buf.toString();
+        //console.log(stats);
+        return stats;
+    }
+
+    toString () {
+        return this.buf.toString();
+    }
+
+    private getMetadata (buf:Buffer) {
+        let metadata:Array<string>;
+        
+        let match:RegExpMatchArray;
+        let idxStart:number, idxEnd:number;
+        let str:string;
+
+        metadata = [];
+
+        // Mvd
+        idxStart = this.buf.indexOf("Server starts recording");
+        idxEnd = this.buf.indexOf(".mvd", idxStart) + 4;
+        str = this.buf.subarray(idxStart, idxEnd).toString();
+        if (match = str.match(/Server starts recording \([a-z]+\)\:\s(.+\.mvd)/mi)) {
+            metadata.push(match[0].replace(/[\r\n]+/g, ' ').trim());
+        }
+
+        // Matchdate
+        idxStart = this.buf.indexOf("matchdate:");
+        idxEnd = this.buf.indexOf("\n", idxStart);
+        str = this.buf.subarray(idxStart, idxEnd).toString()
+        if (match = str.match(/matchdate\: ([0-9]{4}\-[0-9]{2}\-[0-9]{2}\s[0-9]{2}\:[0-9]{2}\:[0-9]{2}\s[a-z]{0,5})/mi)) {
+            metadata.push(match[0]);
+        }
+        
+        return metadata;
+    }
+
+    private getEndGameStats(buf:Buffer) {
+        const idxMatchOver = buf.indexOf("The match is over");
+        const idxFinalScores = buf.indexOf("//finalscores");
+        const idxJsonStart = buf.indexOf('{"version":');
+        const idxEndStats = idxJsonStart > 0 ? idxJsonStart : idxFinalScores;
+
+        // Slice out end game stats to subarray
+        return buf.subarray(idxMatchOver, idxEndStats);
+    }
+
+    private stripControlChars (buf:Buffer) {
+        let newBufferChars = [];
+        let i:number;
+
+        for (i = 0; i < buf.length; i++) {
+            if (STRIP_CHARS.includes(buf[i])) continue;
+            newBufferChars.push(buf[i])
+        }
+
+        return Buffer.from(newBufferChars);
+    }
+
+    private parseMvd() {
+        const buf = this.buf;
+
+        // Parse the stats quake chars
+        let i = 0;
+        for(i = 0; i < buf.length; i++) {
+            buf[i] = this.parseQuakeChar(buf[i], i, buf);
+        }
+    }
+
+    private parseQuakeChar (charcode:number, i?:number, buf?:Buffer) {
+        // Normalize "red chars", secondary color in charset
+        // Probably the entire range 143-160 is -128, but not confirmed
+        if (charcode > 143 && charcode < 157) charcode -= 128;
+        else if (charcode > 160 && charcode < 255) charcode -= 128;
+
+        // Nothing wrong with normal ASCII-chars
+        if (charcode > 31 && charcode < 127) return charcode;
+        else if (CHARSET.hasOwnProperty(charcode)) return CHARSET[charcode];
+        else {
+            const pre = buf.subarray(i-5, i).toString();
+            const is = buf.subarray(i, i+1).toString();
+            const suf = buf.subarray(i+1, i+5).toString();
+    
+            console.log(i + ":" + charcode, pre, clr.bgRed(is), suf)
+            //console.log("%d: char (%d) not found, %s", i, charcode, helpstring)
+            return charcode;
+        }
+    }
+}
 
 export class MatchData implements MatchDataObj {
     id = ""
@@ -79,7 +233,7 @@ class LineCursor  {
         this.len = this.lines.length
         this.n = 0;
 
-        console.log("lines", this.len)
+        //console.log("lines", this.len)
     }
 
     line () { return this.lines[this.n]; }
@@ -100,7 +254,7 @@ class LineParser {
     re_MatchDate = /^matchdate\:\s([0-9]{4}\-[0-9]{2}\-[0-9]{2}\s[0-9]{2}\:[0-9]{2}\:[0-9]{2}\s[a-z]+)$/i
 
     re_PlayerTeam = /^Team\s\[(.+)\]\:$/i
-    re_Player = /^\_\s(.+)\:$/i
+    re_Player = /^\#\s(.+)\:$/i
     re_Player_1 = /^([0-9\-]+)\s\(([0-9\-]+)\)\s([0-9]+)\s([0-9.%]+)$/
     re_Player_2 = /^Wp\:\s([a-z0-9.%]+)?(?:\s?\([0-9]+\/[0-9]+\))?\s?([a-z0-9.%]+)?\s?([a-z0-9.%]+)?\s?([a-z0-9.%]+)?\s?([a-z0-9.%]+)?$/i
     re_Player_3 = /^([a-z\s\&]+)\:\s([a-z]+\:[0-9.]+)\s?([a-z]+\:[0-9.]+)?\s?([a-z]+\:[0-9.]+)?\s?([a-z]+\:[0-9.]+)?\s?([a-z]+\:[0-9.]+)?\s?([a-z]+\:[0-9.]+)?/i
@@ -262,26 +416,24 @@ class LineParser {
 
 function Log_ParseFile (fpath:string) {
     const fileInfo = path.parse(fpath);
-    //console.log(fileInfo)
+    console.log("  →", clr.white(`parsing ${fileInfo.name}.mvd`))
+
     const buf = fs.readFileSync(fpath);
     const mvd = new Mvd_Parser(buf);
     const endStats = mvd.getStats();
     //console.log(endStats);
-    
-    
+        
     const lc = new LineCursor(Buffer.from(endStats));
     const md = new MatchData(emptyMatchDataObj());
-
     const lineParser = new LineParser(lc, md);
-
-    let data = lineParser.parse();
-
-
-    console.log(data)
+    const data = lineParser.parse();
+    //console.log(data)
 
     // WriteJSON
     const outputPath = path.join(process.cwd(), fileInfo.name + ".json");
     fs.writeFileSync(outputPath, JSON.stringify(data, null, 2));
+    console.log("  →", clr.green(`writing ${fileInfo.name}.json`));
+    console.log();
     
     //console.log(data)
 }
@@ -296,9 +448,9 @@ function Log_ParseFiles (fpath:string) {
 
 function main () {
     try {
-        if (!process.argv[2]) throw new Error("usage: node logparser.mjs <logfile|logdir>")
+        if (!process.argv[2]) throw new Error("usage: node mvd-to-json.js <demofile.mvd|demodir>")
         const fpath = path.join(process.cwd(), process.argv[2])
-        if (!fs.existsSync(fpath)) throw new Error("logfile or directory not found")
+        if (!fs.existsSync(fpath)) throw new Error("demofile or directory not found")
         
         const fstat = fs.lstatSync(fpath)
         if (fstat.isDirectory()) Log_ParseFiles(fpath)
