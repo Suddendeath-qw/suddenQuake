62ae669a964e2421acc556e915bad719c782dd81
riovv <patric.nordmark@gmail.com>
2025-12-28 17:44:39 +0100

    misc

--- qw/suddenquotes.js
+++ qw/suddenquotes.js
@@ -22,26 +22,9 @@ try {
             appendFileSync(cfgFileName, alias);
         });
        
-        // Rand for player
-        appendFileSync(cfgFileName, `alias q_${p}_rand "_.rand ${quotes[p].length}; wait; wait; q_${p}$_q_nrand"\n`);
         appendFileSync(cfgFileName, `\n`);
     });
 
-    // Numbered quoutes for rand
-    appendFileSync(cfgFileName, `\n\n// numbered quoutes for rnd\n`);
-    appendFileSync(cfgFileName, `set _q_count ${allQuotes.length}\n`);
-    allQuotes.forEach((q, i) => {
-        let alias = `alias _q_${i} "say ${q}"\n`;
-        appendFileSync(cfgFileName, alias);
-    });
-
-    // rnd alias
-    appendFileSync(cfgFileName, `\n\n// rnd q alias\n`);
-    appendFileSync(cfgFileName, `set _q_nmax "10000000"\n`);
-    appendFileSync(cfgFileName, `alias _.rand "set_calc _q_nrand $rand * %1; _.randround"\n`);
-    appendFileSync(cfgFileName, `alias _.randround "set_calc _q_nrand2 $_q_nrand + $_q_nmax; set_calc _q_nrand2 $_q_nrand2 - $_q_nmax; _.randfloor"\n`);
-    appendFileSync(cfgFileName, `alias _.randfloor "if ($_q_nrand2 > $_q_nrand) then set_calc _q_nrand $_q_nrand2 - 1 else set _q_nrand $_q_nrand2"\n`);
-    appendFileSync(cfgFileName, `alias q_rand "_.rand $_q_count; wait; wait; _q_$_q_nrand"\n`);
 
     console.log(`Generated ${allQuotes.length} quotes by ${Object.keys(quotes).length} players.`)
 } catch (err) {
